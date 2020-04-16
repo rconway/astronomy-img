a125b1d6d4bf1f197e8c2fec0502384317758075
Richard Conway <richard@rconway.co.uk>
2020-04-16 21:48:13 +0100

    Rearchitected to use 'ImageFetcher' objects.

---
+++ imagefetcher/image-fetcher.go
@@ -0,0 +1,6 @@
+package imagefetcher
+
+// ImageFetcher Interface for types that can fetch an image
+type ImageFetcher interface {
+	Fetch() string
+}
--- main.go
+++ main.go
@@ -1,99 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
-	"time"
-)
 
-var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+	"github.com/rconway/astronomy-img/imagefetcher"
+)
 
 func init() {
 	log.Println("...astronomy-img...")
 }
 
-func check(err error) {
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-// get image URL by requestng json metadata from site
-func getImageURL(siteURL string) (string, error) {
-	myClient := &http.Client{Timeout: 10 * time.Second}
-
-	// get metadata from server
-	resp, err := myClient.Get(siteURL)
-	check(err)
-	defer resp.Body.Close()
-	fmt.Println("Response status:", resp.Status)
-
-	// parse the json response
-	type responseData struct {
-		URL *string `json:"url"`
-	}
-	data := &responseData{}
-	err = json.NewDecoder(resp.Body).Decode(data)
-	check(err)
-
-	// error is url is missing from json response
-	if data.URL == nil {
-		return "", fmt.Errorf("'url' missing from json response")
-	}
-
-	// all good - return the image url
-	return *data.URL, nil
-}
-
-func getImage(url string, fileName string) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	myClient := &http.Client{Timeout: 10 * time.Second}
-	resp, err := myClient.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	_, err = io.Copy(file, resp.Body)
-
-	return err
-}
-
-// get the filename from the end of the url
-func buildFileName(fullURLFile string) (string, error) {
-	var fileName = ""
-
-	fileURL, err := url.Parse(fullURLFile)
-	if err != nil {
-		return fileName, err
-	}
-
-	path := fileURL.Path
-	segments := strings.Split(path, "/")
-	fileName = segments[len(segments)-1]
-
-	return fileName, nil
-}
-
 func main() {
-	// image URL
-	imageURLStr, err := getImageURL(nasaURL)
-	check(err)
-	log.Printf("Image URL is %v\n", imageURLStr)
+	var fetcher imagefetcher.ImageFetcher
+	fetcher = &imagefetcher.NasaImageFetcher{}
+	filename := fetcher.Fetch()
 
-	// image itself
-	imageFilename, err := buildFileName(imageURLStr)
-	check(err)
-	getImage(imageURLStr, imageFilename)
+	log.Printf("Image retrieved to filename: %v\n", filename)
 }
